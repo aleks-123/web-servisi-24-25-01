3494d8737eaff92a8137002a06b870a46daf3948
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2025-03-12 20:47:09 +0100

    sendEmail

---
+++ c1/handler/emailHandler.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  //1 Kreiranje na transporter
+
+  // //! Gmail
+  // const transporter = nodemailer.createTransport({
+  //   service: 'Gmail',
+  //   auth: {
+  //     user: 'aleksandar@gmail.com',
+  //     pass: '123456789!Q',
+  //   },
+  // });
+  //! manuelen email provider
+  const transporter = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: 'b881a174e483e8',
+      pass: 'f2fb94f79b09d5',
+    },
+  });
+
+  transporter.verify((err, succ) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('success');
+    }
+  });
+
+  // 2) definiranje na opciite na mailot
+  const mailOptions = {
+    from: 'Apple <apple@apple.com',
+    to: options.email,
+    subject: options.subject,
+    text: options.messages,
+  };
+
+  // 3) go isprame mailot
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
